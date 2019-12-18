67416477f394b0979e76632a01fe9c6b06d474f6
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 13:51:50 -0500

    creates extension file for constraints for FoodHistoryVC

---
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/FoodHistoryVC+Constraints.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  foodprint-hackathon-app
+//
+//  Created by Sunni Tang on 12/18/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+extension FoodDetailsVC {
+    //MARK: - UI Constraints
+    
+}
