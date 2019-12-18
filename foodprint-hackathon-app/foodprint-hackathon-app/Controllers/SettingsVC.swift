7c4a36241ca508fa31a2b56d6b668fd155e48593
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 14:55:55 -0500

    sets stackview distribution, axis, and aligntment and calls addSubviews and addConstraints func in settingsVC

--- foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC.swift
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC.swift
@@ -13,31 +13,39 @@ class SettingsVC: UIViewController {
     //TODO: Refactor later to change input method (i.e. picker?)
     lazy var nameTextField: UITextField = {
         let textField = UITextField()
+        textField.placeholder = "Enter Name"
         return textField
     }()
     
     lazy var heightTextField: UITextField = {
         let textField = UITextField()
+        textField.placeholder = "Enter Height"
         return textField
     }()
     
     lazy var weightTextField: UITextField = {
         let textField = UITextField()
+        textField.placeholder = "Enter Weight"
         return textField
     }()
     
     lazy var submitButton: UIButton = {
         let button = UIButton()
+        button.setTitle("Enter", for: .normal)
         return button
     }()
     
     lazy var inputStackView: UIStackView = {
        let stackView = UIStackView(arrangedSubviews: [nameTextField, heightTextField, weightTextField, submitButton])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
         return stackView
     }()
     
     lazy var calorieGoalLabel: UILabel = {
         let label = UILabel()
+        label.text = "Calorie Goal"
         return label
     }()
     
@@ -48,6 +56,7 @@ class SettingsVC: UIViewController {
     
     lazy var emissionsGoalLabel: UILabel = {
         let label = UILabel()
+        label.text = "Emissions Goal"
         return label
     }()
     
@@ -58,13 +67,18 @@ class SettingsVC: UIViewController {
     
     lazy var goalStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [calorieGoalLabel, calorieSlider, emissionsGoalLabel, emissionsSlider])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
         return stackView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .yellow
+        
+        addSubviews()
+        addConstraints()
     }
 
 
