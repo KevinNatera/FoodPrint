f73a0df52ef7c8818920d2a98fcab4def46a394e
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 16:33:49 -0500

    adds UI lazy objects to homescreenVC

---
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/HomescreenVC+Constraints.swift
@@ -0,0 +1,9 @@
+//
+//  HomescreenVC+Constraints.swift
+//  foodprint-hackathon-app
+//
+//  Created by Sunni Tang on 12/18/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
--- foodprint-hackathon-app/foodprint-hackathon-app/Controllers/HomescreenVC.swift
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/HomescreenVC.swift
@@ -9,22 +9,71 @@
 import UIKit
 
 class HomescreenVC: UIViewController {
-
+    //TODO: Show alert to pop to SettingsVC if info is not complete
+    lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hi name!"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var calorieGoalLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Calories per day: "
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var emissionsGoalLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Emissions per day: "
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var userInfoStackView: UIView = {
+        let stackView = UIStackView(arrangedSubviews: [welcomeLabel, calorieGoalLabel, emissionsGoalLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    lazy var progressLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Progress"
+        return label
+    }()
+    
+    lazy var calorieProgressView: UIProgressView = {
+        let progressView = UIProgressView()
+        return progressView
+    }()
+    
+    lazy var emissionsProgressView: UIProgressView = {
+        let progressView = UIProgressView()
+        return progressView
+    }()
+    
+    lazy var progressStackView: UIView = {
+        let stackView = UIStackView(arrangedSubviews: [progressLabel, calorieProgressView, emissionsProgressView])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    lazy var addFoodButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add Food", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
