83a5c1253ce6cda7b6ff38d4cc6bf3dfae6b4c86
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 14:50:20 -0500

    adds objects to subview with constraints in settingsVC+constraints

--- foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC+Constraints.swift
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC+Constraints.swift
@@ -7,7 +7,41 @@
 //
 
 import Foundation
+import UIKit
 
 extension SettingsVC {
+    //MARK: - UI Constraints
+    func addSubviews() {
+        view.addSubview(inputStackView)
+        view.addSubview(goalStackView)
+    }
     
+    func addConstraints() {
+        setInputStackViewConstraints()
+        setGoalStackViewConstraints()
+    }
+    
+    private func setInputStackViewConstraints() {
+        inputStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            inputStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            inputStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            inputStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            inputStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+        
+    }
+    
+    private func setGoalStackViewConstraints() {
+        goalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            goalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            goalStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            goalStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            goalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
 }
