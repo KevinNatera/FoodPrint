e07ae8f6e4ea2e6e5f512b0507881bdd7aa46489
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 14:35:07 -0500

    creates settingsVC+constraints file

---
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC+Constraints.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsVC+Constraints.swift
+//  foodprint-hackathon-app
+//
+//  Created by Sunni Tang on 12/18/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+extension SettingsVC {
+    
+}
