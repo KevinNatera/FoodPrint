eba184d85adf7f20ad5a3a88cec72fd957838faf
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 12:27:55 -0500

    removes storyboard references and sets SettingsVC as the root view controller

--- foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC.swift
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC.swift
@@ -13,6 +13,7 @@ class SettingsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
 
 
