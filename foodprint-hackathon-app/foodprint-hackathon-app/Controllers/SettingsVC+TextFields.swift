8d665028f3276392d568182790febcdeec858e92
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-12-18 19:28:35 -0500

    creates settingsVC+textFields extension

---
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/SettingsVC+TextFields.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsVC+TextFields.swift
+//  foodprint-hackathon-app
+//
+//  Created by Sunni Tang on 12/18/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension SettingsVC: UITextFieldDelegate {
+    
+    
+}
