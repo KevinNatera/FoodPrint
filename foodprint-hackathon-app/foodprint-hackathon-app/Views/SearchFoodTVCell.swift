9efd2711799a21fe2215b64881f8b46c19cc6d3d
Kyang646 <kimballyang@pursuit>
2019-12-19 14:25:31 -0500

    Fixed UI for SearchFoodTVCell

--- foodprint-hackathon-app/foodprint-hackathon-app/Views/SearchFoodTVCell.swift
+++ foodprint-hackathon-app/foodprint-hackathon-app/Views/SearchFoodTVCell.swift
@@ -13,7 +13,7 @@ class SearchFoodTVCell: UITableViewCell {
     //MARK: - Properties
     lazy var foodNameLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .green
+        label.backgroundColor = .lightGray
         //label.frame = CGRect(x: 0, y: 0, width: 200, height: 150)
         label.text = "foodName"
         label.textAlignment = .center
@@ -22,7 +22,7 @@ class SearchFoodTVCell: UITableViewCell {
     
     lazy var caloriesPerServingLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .blue
+        label.backgroundColor = .lightGray
         //label.frame = CGRect(x: 200, y: 0, width: 250, height: 75)
         label.text = "caloriesPerServing"
         label.textAlignment = .center
@@ -31,7 +31,7 @@ class SearchFoodTVCell: UITableViewCell {
     
     lazy var emissionsPerServingLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .red
+        label.backgroundColor = .lightGray
         //label.frame = CGRect(x: 200, y: 75, width: 250, height: 75)
         label.text = "emissionsPerServing"
         label.textAlignment = .center
