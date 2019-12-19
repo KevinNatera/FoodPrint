fb0b6e975d15bcaab41bf3f681330be2a86b3ab0
Kevin Natera <mycityneedsme@Dat-Boiis-Toaster.local>
2019-12-19 13:48:52 -0500

    Created FoodHistoryVC + constraints

--- foodprint-hackathon-app/foodprint-hackathon-app/Controllers/FoodHistoryVC+Constraints.swift
+++ foodprint-hackathon-app/foodprint-hackathon-app/Controllers/FoodHistoryVC+Constraints.swift
@@ -7,8 +7,24 @@
 //
 
 import Foundation
+import UIKit
 
-extension FoodDetailsVC {
+extension FoodHistoryVC {
     //MARK: - UI Constraints
-    
+    func addSubViews() {
+        view.addSubview(historyTableView)
+    }
+
+    func addConstraints() {
+        historyTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+        historyTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        historyTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+        historyTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        historyTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        
+        ])
+    }
 }
